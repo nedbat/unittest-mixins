8cf2eec0be0eb52c12106d589a0de2e1ce58930c
Ned Batchelder <ned@nedbatchelder.com>
2017-03-30 06:42:35 -0400

    v1.3

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ Development Status :: 5 - Production/Stable
 
 setup(
     name='unittest-mixins',
-    version='1.2.1',
+    version='1.3',
     description='Helpful mixins for unittest classes',
     author='Ned Batchelder',
     author_email='ned@nedbatchelder.com',
